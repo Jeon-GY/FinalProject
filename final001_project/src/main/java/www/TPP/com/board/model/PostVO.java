07cfc216066f0eccde6420fed11836bf6512ae90
Jeon-GY <rkdud22312@gmail.com>
2021-07-05 22:06:21 +0900

    20210705프로젝트

---
+++ final001_project/src/main/java/www/TPP/com/board/model/PostVO.java
@@ -0,0 +1,18 @@
+package www.TPP.com.board.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import www.TPP.com.common.model.CommonAttr;
+import www.TPP.com.party.model.Party;
+
+@Data
+@NoArgsConstructor
+public class PostVO extends CommonAttr{
+	private String postId;	
+	private String postTitle;
+	private String sellCount;
+	private double rate;
+	
+	private Party seller;
+}
+
